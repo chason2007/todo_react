fe0884577f2c392a422417881accf5a7bf1938e1
chason2007 <chasonchevyhurtis.2024cse@sece.ac.in>
2025-12-10 14:28:16 +0530

    created page files

---
+++ src/Pages/Settings.js
@@ -0,0 +1,10 @@
+const Settings = () => {
+    return (
+        <div>
+            <h1>Settings Page</h1> 
+            <p>Manage your application settings here.</p>
+        </div>
+    );
+}
+
+export default Settings;
