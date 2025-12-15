22f00180bc3550351be065c647a13c2d14e58aaf
chason2007 <chasonchevyhurtis.2024cse@sece.ac.in>
2025-12-15 13:03:24 +0530

    get by ID working using iteration method

--- my-project/index.js
+++ my-project/index.js
@@ -4,37 +4,40 @@ const fs = require("fs");
 
 app.use(express.json());
 
-const jsonData = JSON.parse(fs.readFileSync(
-    "./Data.json"
-))
+const jsonData = JSON.parse(fs.readFileSync("./Data.json"));
 
 //get
-app.get("/api/v1/rest", (req, res)=>{
-    res.status(200).json({
-        status: "Sucessful",
-        length: jsonData.length,
-        data: {
-            jsonData,
-        }
-    })
+app.get("/api/v1/rest", (req, res) => {
+  res.status(200).json({
+    status: "Sucessful",
+    length: jsonData.length,
+    data: {
+      jsonData,
+    },
+  });
 });
 
-// app.get("/api/v1/rest/:id", (req, res)=>{
-//     let id = parseInt(req.params.id);
-//     let restaurant = jsonData.find((el)=>el.id===id);
-//     if(!restaurant){
-//         return res.status(404).json({
-//             status: "failed",
-//             message: "Restaurant not found"
-//         })
-//     }
-//     res.status(200).json({
-//         status: "Successful",
-//         data: restaurant
-//     })
-// })
+app.get("/api/v1/rest/:id", (req, res) => {
+  let id = req.params.id;
+  let restaurant = null;
+  for (let i = 0; i < jsonData.length; i++) {
+    let items = jsonData[i].card.card.itemCards;
+    restaurant = items.find((el) => el.card.info.id == id);
+    if (restaurant) break;
+  }
 
-app.listen(9000, ()=>
-{
-    console.log("Server started successfully"); 
-})
+  if (!restaurant) {
+    return res.status(404).json({
+      status: "Fail",
+      message: "user not found",
+    });
+  }
+  res.status(200).json({
+    status: "success",
+    restaurant,
+  });
+});
+
+app.listen(3001, () => {
+  console.log("Server started successfully on port 3001");
+});
