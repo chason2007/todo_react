f3ad7c7a5b34baeb89dc8c013e10ee0542cdcd7d
chason2007 <chasonchevyhurtis.2024cse@sece.ac.in>
2025-12-15 14:04:25 +0530

    added routes and controllers

--- my-project/index.js
+++ my-project/index.js
@@ -6,6 +6,39 @@ app.use(express.json());
 
 const jsonData = JSON.parse(fs.readFileSync("./Data.json"));
 
+
+//controllers
+const getAllRestaurants = (req, res) => {
+  res.status(200).json({
+    status: "Sucessful",
+    length: jsonData.length,
+    data: {
+      jsonData,
+    },
+  });
+};
+
+const getRestaurantById = (req, res) => {
+  let id = req.params.id;
+  let restaurant = null;
+  for (let i = 0; i < jsonData.length; i++) {
+    let items = jsonData[i].card.card.itemCards;
+    restaurant = items.find((el) => el.card.info.id == id);
+    if (restaurant) break;
+  }
+
+  if (!restaurant) {
+    return res.status(404).json({
+      status: "Fail",
+      message: "user not found",
+    });
+  }
+  res.status(200).json({
+    status: "success",
+    restaurant,
+  });
+};
+
 //get
 app.get("/api/v1/rest", (req, res) => {
   res.status(200).json({
@@ -16,6 +49,15 @@ app.get("/api/v1/rest", (req, res) => {
     },
   });
 });
+
+//Routes
+app.get("/api/v1/rest", getAllRestaurants){
+    app.get("/api/v1/rest/:id", getRestaurantById);
+    app.post("/api/v1/rest", createRestaurant);
+    app.patch("/api/v1/rest/:id", updateRestaurant);
+    app.delete("/api/v1/rest/:id", deleteRestaurant);
+
+}
 
 app.get("/api/v1/rest/:id", (req, res) => {
   let id = req.params.id;
